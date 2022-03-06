7472fcae6581b283c4205c6bf01a0396eecfe36b
Clayton S. Morais <clayton.s.morais@gmail.com>
2022-03-05 21:23:02 -0300

    nova versao

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
-from flask import send_from_directory
+from flask import send_from_directory,render_template
 from main import process_file
+import googlemaps
+geolocate = googlemaps.Client(key='AIzaSyAc-r4Q2x3OLSvJgXZT7BnsCAbit8rSnog')
+
+import openpyxl
 
 
 UPLOAD_FOLDER = os.path.join('arquivos')
@@ -11,9 +15,34 @@ ALLOWED_EXTENSIONS = {'xlsx'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/uploads/<name>')
-def download_file(name):
-    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+@app.route('/resultado')
+def resultado():
+    locations = []
+    arquivo = os.path.join(app.config["UPLOAD_FOLDER"], 'retorno.xlsx')
+    wb_obj = openpyxl.load_workbook(arquivo)
+    sheet_obj = wb_obj.active
+    for x in sheet_obj.iter_rows(min_row=2, max_row=sheet_obj.max_row, min_col=1, max_col=6):
+        location = '{},{}'.format(x[1].value,x[0].value)
+        distancia_sorocaba = '{} Km'.format(round(x[3].value/1000,2))
+        info = x[5].value
+        locations.append({'location':location,
+                          'code':None,
+                          'distancia':distancia_sorocaba,
+                          'info':info
+                          })
+
+    sorocaba = geolocate.geocode(address='Sorocaba,SP',language='pt_BR')
+    sorocaba = sorocaba[0]['geometry']['location']
+
+    for y,x in enumerate(locations):
+        location = geolocate.geocode(address=x['location'],language='pt_BR')
+        locations[y]['code']=location[0]['geometry']['location']
+
+    return render_template('resultado.html',sorocaba=sorocaba,locacoes=locations)
+
+@app.route('/uploads')
+def download_file():
+    return send_from_directory(app.config["UPLOAD_FOLDER"], 'retorno.xlsx')
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -37,15 +66,9 @@ def upload_file():
             arquivo = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(arquivo)
             process_file(arquivo)
-            return redirect(url_for('download_file', name='retorno.xlsx'))
-    return '''
-        <!doctype html>
-        <title>Upload new File</title>
-        <h1>Upload new File</h1>
-        <form method=post enctype=multipart/form-data>
-          <input type=file name=file>
-          <input type=submit value=Upload>
-        </form>
-        '''
+            #return redirect(url_for('download_file', name='retorno.xlsx'))
+            return redirect(url_for('resultado'))
+    return render_template('base.html')
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
---
+++ static/main.js
@@ -0,0 +1,45 @@
+
+
+
+$(document).ready(function(){
+
+    $('#file').on('change',function(){
+        $('#arq_selecionado').text($(this)[0].files[0].name);
+    });
+
+    $('input[type="submit"]').on('click',function (e) { 
+        e.preventDefault();
+
+        if($('#file')[0].files.length==0){
+            $('#staticBackdrop').modal('show');
+        }else{
+            $('form').submit();
+        }
+     })
+
+
+
+});
+
+function initMap() {
+    // The location of Uluru
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("mapa"), {
+        zoom: 8,
+        center: sorocaba,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: sorocaba,
+        map: map,
+        title:"Sorocaba,SP"
+    });
+
+    locations.forEach(element => {
+        new google.maps.Marker({
+            position: element.code,
+            title:`element.location\nDistância de Sorocaba:${element.distancia}\nInfo:${element.info}`,
+            map: map,
+        });
+    });
+}     
